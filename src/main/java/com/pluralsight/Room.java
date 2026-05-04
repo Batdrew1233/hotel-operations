896ef38e796eefa7d6a0e4b3654480f219ac2b7e
Batdrew1233 <drewstevenson1233@gmail.com>
2026-05-04 13:26:08 -0400

    Created getter for Room class

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
 public class Room {
+    private int numberOfBeds;
+    private long price;
+    private boolean Occupied;
+    private boolean dirty;
+    private boolean available;
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return Occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
 }
